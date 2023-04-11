835b748243b49e316ca455d61961b564555e614a
Haf <haf@protonmail.ch>
2023-04-11 07:54:12 +0200

    toPairOfNullables

--- src/main/kotlin/xyz/haff/koy/collections/PairExtensions.kt
+++ src/main/kotlin/xyz/haff/koy/collections/PairExtensions.kt
@@ -3,3 +3,21 @@ package xyz.haff.koy.collections
 inline fun <T, R> Pair<T, T>.map(f: (T) -> R): Pair<R, R> {
     return f(this.first) to f(this.second)
 }
+
+/**
+ * Allows for using decomposition with null pairs:
+ *  * If the pair is not null, it just returns it.
+ *  * Otherwise, it returns a pair of nulls.
+ *
+ *  Example:
+ *  ```kotlin
+ *  // This is impossible
+ *  val pair: Pair<String, String>? = null
+ *  val (first, second) = null
+ *  // But now we can do this
+ *  val (first, second) = null.toPairOfNullables()
+ *  ```
+ */
+fun <T, R> Pair<T, R>?.toPairOfNullables(): Pair<T?, R?> {
+    return this ?: (null to null)
+}
--- src/test/kotlin/xyz/haff/koy/collections/PairExtensionsTest.kt
+++ src/test/kotlin/xyz/haff/koy/collections/PairExtensionsTest.kt
@@ -8,4 +8,12 @@ class PairExtensionsTest : FunSpec({
     test("map") {
         ("1" to "2").map { it.toInt() } shouldBe (1 to 2)
     }
+
+    test("toPairOfNullables") {
+        val pair: Pair<String, String>? = null
+        val (first, second) = pair.toPairOfNullables()
+
+        first shouldBe null
+        second shouldBe null
+    }
 })
